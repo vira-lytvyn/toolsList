32bf5bf2c54ab61ac821bf3f1a757906161d691b
Vira Lytvyn <baterfluy@gmail.com>
2015-03-22 23:48:07 +0200

    Added a few tools. Removed logs.

--- js/db.js
+++ js/db.js
@@ -16,7 +16,7 @@
             this.tools.push(insertingTool);
         },
 
-        updateItem: function(updatingTool) { },
+        updateItem: function(updatingTool) { console.log(updatingTool);},
 
         deleteItem: function(deletingTool) {
             var toolIndex = $.inArray(deletingTool, this.tools);
@@ -27,22 +27,63 @@
 
     window.db = db;
 
-
     db.classes = [
         { Name: "", Id: 0 },
-        { Name: "Libraries", Id: 1 },
-        { Name: "Editors", Id: 2 },
-        { Name: "Frameworks", Id: 3 },
-        { Name: "Technologies", Id: 4 },
+        { Name: "Library", Id: 1 },
+        { Name: "Editor", Id: 2 },
+        { Name: "Framework", Id: 3 },
+        { Name: "Technology", Id: 4 },
         { Name: "Approach", Id: 5 },
         { Name: "Meinstream", Id: 6 },
-        { Name: "Person", Id: 7 }
+        { Name: "Person", Id: 7 },
+        { Name: "Plugin", Id: 8 }        
     ];
 
     db.tools = [
+        {
+            "Name": "is.js",
+            "Website": "",
+            "Class": 1,
+            "Used": false,
+            "Mark": 0
+        },
+        {
+            "Name": "jsGrid.js",
+            "Website": "",
+            "Class": 8,
+            "Used": true,
+            "Mark": 7
+        },
+        {
+            "Name": "Brackets",
+            "Website": "",
+            "Class": 2,
+            "Used": false,
+            "Mark": 0
+        },
+        {
+            "Name": "Atom",
+            "Website": "",
+            "Class": 2,
+            "Used": false,
+            "Mark": 0
+        },
+        {
+            "Name": "raw.js",
+            "Website": "",
+            "Class": 3,
+            "Used": false,
+            "Mark": 0
+        }
     ];
 
     db.users = [
+        {
+            "ID": "x",
+            "Account": "A758A693-0302-03D1-AE53-EEFE22855556",
+            "Name": "Vira Lytvyn",
+            "RegisterDate": "2015-03-22T23:41:52-07:00"
+        }
     ];
 
 }());
